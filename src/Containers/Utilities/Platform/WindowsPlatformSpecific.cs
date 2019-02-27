ea87772f8cb29093cd9fae3e4ca5793ad2dc5145
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-02-27 12:25:35 +0800

    windows docker

--- src/Containers/Utilities/Platform/WindowsPlatformSpecific.cs
+++ src/Containers/Utilities/Platform/WindowsPlatformSpecific.cs
@@ -9,6 +9,6 @@ namespace TestContainers.Containers.Utilities.Platform
         //public string TinyDockerImage { get; } = "mcr.microsoft.com/windows/nanoserver:1809";
         public string TinyDockerImage { get; } = "mcr.microsoft.com/windows/nanoserver:sac2016";
         
-        public string RyukImage { get; } = "ryuk:0.2.3-windows-sac2016";
+        public string RyukImage { get; } = "ryuk:0.2.3-nanoserver-sac2016";
     }
 }
