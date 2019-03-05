8d56d823332064a251be9c418386a37e368628aa
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-03-05 13:16:31 +0800

    Update release tag regex to include pre-releases

--- remote-publish.sh
+++ remote-publish.sh
@@ -4,14 +4,11 @@ set -e
 
 API_KEY=$1
 SOURCE=$2
+CONFIGURATION="Release"
 
 PROJECT_DIR="src/Container.Abstractions"
 CSPROJ="${PROJECT_DIR}/Container.Abstractions.csproj"
 ASSEMBLY_NAME="TestContainers.Container.Abstractions"
-CONFIGURATION="Release"
-
-echo "dotnet pack ${CSPROJ} -c ${CONFIGURATION}"
-echo "dotnet nuget push ${PROJECT_DIR}/bin/${CONFIGURATION}/${ASSEMBLY_NAME}.*.nupkg -s ${SOURCE} -k ${API_KEY}"
 
 dotnet pack ${CSPROJ} -c ${CONFIGURATION}
 dotnet nuget push ${PROJECT_DIR}/bin/${CONFIGURATION}/${ASSEMBLY_NAME}.*.nupkg -s ${SOURCE} -k ${API_KEY}
