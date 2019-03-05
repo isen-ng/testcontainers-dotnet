6b4b1402395eca56fa3f1be941c29c1f2b63891f
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-03-05 11:11:17 +0800

    Properly format comment

--- src/Container.Abstractions/Utilities/Platform/WindowsPlatformSpecific.cs
+++ src/Container.Abstractions/Utilities/Platform/WindowsPlatformSpecific.cs
@@ -4,9 +4,15 @@ namespace TestContainers.Container.Abstractions.Utilities.Platform
     {
         public static IPlatformSpecific Instance { get; } = new WindowsPlatformSpecific();
 
-        // https://github.com/appveyor/ci/issues/2466
-        // https://github.com/appveyor/ci/issues/1885
-        //public string TinyDockerImage { get; } = "mcr.microsoft.com/windows/nanoserver:1809";
+        /*
+         * AppVeyor still uses a sac 2016 image and running any higher version docker
+         * image will result in error
+         * https://github.com/appveyor/ci/issues/1885
+         * https://github.com/appveyor/ci/issues/1885
+         *
+         * https://github.com/isen-ng/testcontainers-dotnet/issues/14
+         * Todo: Upgrade image to "mcr.microsoft.com/windows/nanoserver:1809"
+         */
         public string TinyDockerImage { get; } = "mcr.microsoft.com/windows/nanoserver:sac2016";
 
         public string RyukImage { get; } = "ryuk:0.2.3-nanoserver-sac2016";
