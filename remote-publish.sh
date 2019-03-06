c75555ccdc847eb2c381940d6695a49186901b2e
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-03-06 17:26:39 +0800

    Add projects to remote publish

--- remote-publish.sh
+++ remote-publish.sh
@@ -6,9 +6,31 @@ API_KEY=$1
 SOURCE=$2
 CONFIGURATION="Release"
 
+function remote_publish() {
+    dotnet pack $1 -c ${CONFIGURATION}
+    dotnet nuget push $2/bin/${CONFIGURATION}/$3.*.nupkg -s ${SOURCE} -k ${API_KEY}
+}
+
 PROJECT_DIR="src/Container.Abstractions"
 CSPROJ="${PROJECT_DIR}/Container.Abstractions.csproj"
 ASSEMBLY_NAME="TestContainers.Container.Abstractions"
 
-dotnet pack ${CSPROJ} -c ${CONFIGURATION}
-dotnet nuget push ${PROJECT_DIR}/bin/${CONFIGURATION}/${ASSEMBLY_NAME}.*.nupkg -s ${SOURCE} -k ${API_KEY}
+remote_publish ${CSPROJ} ${ASSEMBLY_NAME} ${PROJECT_DIR}
+
+PROJECT_DIR="src/Container.Database"
+CSPROJ="${PROJECT_DIR}/Container.Database.csproj"
+ASSEMBLY_NAME="TestContainers.Container.Database"
+
+remote_publish ${CSPROJ} ${ASSEMBLY_NAME} ${PROJECT_DIR}
+
+PROJECT_DIR="src/Container.Database.AdoNet"
+CSPROJ="${PROJECT_DIR}/Container.Database.AdoNet.csproj"
+ASSEMBLY_NAME="TestContainers.Container.Database.AdoNet"
+
+remote_publish ${CSPROJ} ${ASSEMBLY_NAME} ${PROJECT_DIR}
+
+PROJECT_DIR="src/Container.Database.PostgreSql"
+CSPROJ="${PROJECT_DIR}/Container.Database.PostgreSql.csproj"
+ASSEMBLY_NAME="TestContainers.Container.Database.PostgreSql"
+
+remote_publish ${CSPROJ} ${ASSEMBLY_NAME} ${PROJECT_DIR}
