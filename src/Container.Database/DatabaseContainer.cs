c5550a21155c9343e0aac93fcaa7b59d22d780b4
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-03-11 16:57:32 +0800

    xml docs for database

--- src/Container.Database/DatabaseContainer.cs
+++ src/Container.Database/DatabaseContainer.cs
@@ -5,14 +5,27 @@ using TestContainers.Container.Database.Hosting;
 
 namespace TestContainers.Container.Database
 {
+    /// <summary>
+    /// Convenience container for db type containers
+    /// </summary>
     public class DatabaseContainer : GenericContainer
     {
+        /// <summary>
+        /// Database name
+        /// </summary>
         public virtual string DatabaseName { get; set; }
         
+        /// <summary>
+        /// Database username
+        /// </summary>
         public virtual string Username { get; set; }
         
+        /// <summary>
+        /// Database password
+        /// </summary>
         public virtual string Password { get; set; }
 
+        /// <inheritdoc />
         public DatabaseContainer(string dockerImageName, IDockerClient dockerClient, ILoggerFactory loggerFactory)
             : base(dockerImageName, dockerClient, loggerFactory)
         {
--- src/Container.Database/Hosting/ContainerBuilderExtensions.cs
+++ src/Container.Database/Hosting/ContainerBuilderExtensions.cs
@@ -2,8 +2,20 @@ using TestContainers.Container.Abstractions.Hosting;
 
 namespace TestContainers.Container.Database.Hosting
 {
+    /// <summary>
+    /// Extensions to configure DatabaseContainers
+    /// </summary>
     public static class ContainerBuilderExtensions
     {
+        /// <summary>
+        /// Configures common db parameters for DatabaseContainers
+        /// </summary>
+        /// <param name="builder">builder</param>
+        /// <param name="username">db username</param>
+        /// <param name="password">db password</param>
+        /// <param name="databaseName">db name</param>
+        /// <typeparam name="T">DatabaseContainer type</typeparam>
+        /// <returns>Builder</returns>
         public static ContainerBuilder<T> ConfigureDatabaseConfiguration<T>(this ContainerBuilder<T> builder,
             string username, string password, string databaseName)
             where T : DatabaseContainer
