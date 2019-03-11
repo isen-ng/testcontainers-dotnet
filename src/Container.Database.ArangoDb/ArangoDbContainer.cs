a6774bb1c9c68065c9caecd5e87890b6efcf55ac
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-03-11 17:01:30 +0800

    Arango docs

--- src/Container.Database.ArangoDb/ArangoDbContainer.cs
+++ src/Container.Database.ArangoDb/ArangoDbContainer.cs
@@ -7,54 +7,77 @@ using Docker.DotNet;
 using Microsoft.Extensions.Logging;
 using TestContainers.Container.Abstractions;
 using TestContainers.Container.Abstractions.WaitStrategies;
-using TestContainers.Container.Database.Hosting;
 
 namespace TestContainers.Container.Database.ArangoDb
 {
+    /// <summary>
+    /// ArangoDb container
+    /// </summary>
+    /// <inheritdoc />
     public class ArangoDbContainer : DatabaseContainer
     {
+        /// <summary>
+        /// Default image name
+        /// </summary>
         public new const string DefaultImage = "arangodb";
+
+        /// <summary>
+        /// Default image tag
+        /// </summary>
         public new const string DefaultTag = "3.4";
+
+        /// <summary>
+        /// Default port db is going to start in
+        /// </summary>
         public const int DefaultPort = 8529;
-        
+
         private const string TestQueryString = "RETURN 1";
-        
+
+        /// <inheritdoc />
         public override string Username => "root";
 
+        /// <inheritdoc />
         public override string DatabaseName => "_system";
 
+        /// <inheritdoc />
         public ArangoDbContainer(IDockerClient dockerClient, ILoggerFactory loggerFactory)
             : base($"{DefaultImage}:{DefaultTag}", dockerClient, loggerFactory)
         {
         }
-        
+
+        /// <inheritdoc />
         public ArangoDbContainer(string dockerImageName, IDockerClient dockerClient, ILoggerFactory loggerFactory)
             : base(dockerImageName, dockerClient, loggerFactory)
         {
         }
 
+        /// <inheritdoc />
         protected override async Task ConfigureAsync()
         {
             if (string.IsNullOrEmpty(Password))
             {
                 throw new InvalidOperationException("Root password cannot null or empty");
             }
-            
+
             await base.ConfigureAsync();
 
             ExposedPorts.Add(DefaultPort);
             Env.Add("ARANGO_ROOT_PASSWORD", Password);
-            
-            WaitStrategy = new ProbingStrategy(Probe, 
+
+            WaitStrategy = new ProbingStrategy(Probe,
                 typeof(HttpRequestException), // when service isn't up yet
                 typeof(InvalidOperationException)); // sometimes http server up but response still empty/null
         }
 
+        /// <summary>
+        /// Gets the arango url required to make commands and queries
+        /// </summary>
+        /// <returns></returns>
         public string GetArangoUrl()
         {
             return $"http://{GetDockerHostIpAddress()}:{GetMappedPort(DefaultPort)}";
         }
-        
+
         private async Task Probe(IContainer container)
         {
             var settings = new DatabaseSharedSetting
