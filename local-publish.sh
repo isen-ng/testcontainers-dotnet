a0c54c4b1384f8bdfc86be0797ee01f6bc58e78d
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-11-13 11:27:17 +0800

    update local-publish

--- local-publish.sh
+++ local-publish.sh
@@ -5,7 +5,7 @@ set -e
 eval SOURCE="~/.nuget/packages"
 
 CONFIGURATION="Release"
-VERSION="1.4.5-SNAPSHOT"
+VERSION="1.4.6-SNAPSHOT"
 
 function local_publish() {
     dotnet pack $1 -c ${CONFIGURATION} -property:Version=${VERSION}
